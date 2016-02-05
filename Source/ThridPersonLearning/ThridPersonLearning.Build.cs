45aa9aeda5d0652401c7d6155b63a3aad315eaca
Shawn Campbell <scampbell@vetsfirstchoice.com>
2016-02-04 23:06:20 -0500

    adding source

---
+++ Source/ThridPersonLearning/ThridPersonLearning.Build.cs
@@ -0,0 +1,11 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ThridPersonLearning : ModuleRules
+{
+	public ThridPersonLearning(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+	}
+}
---
+++ Source/ThridPersonLearning/ThridPersonLearning.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __THRIDPERSONLEARNING_H__
+#define __THRIDPERSONLEARNING_H__
+
+#include "EngineMinimal.h"
+
+#endif
